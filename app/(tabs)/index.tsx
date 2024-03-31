f329e2e24a81f33cf36fea04b9ddb74be947cfc1
Nam <namnv1923@gmail.com>
2024-03-31 17:24:10 +0700

    setup router

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 const Page = () => {
   return (
     <View>
-      <Text>Index</Text>
+      <Link href={"/(modals)/login"}>Login</Link>
+      <Link href={"/(modals)/booking"}>Booking</Link>
+      <Link href={"/listing/2302"}>Listing details</Link>
     </View>
   );
 };
